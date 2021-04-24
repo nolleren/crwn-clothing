daad07bb4b9a940811b4b1d8f3be023b7a818e1b
Stefan Østergaard <Stefan.Ostergaard@tunstall.com>
2021-04-24 14:31:51 +0200

    Added React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
+  <BrowserRouter>
     <App />,
-  document.getElementById('root')
+    document.getElementById('root')
+  </BrowserRouter>
+  
 );
