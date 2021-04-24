c95c5b5717cad524b5630e75ac0a38ba8da40193
Stefan Østergaard <Stefan.Ostergaard@tunstall.com>
2021-04-24 13:57:48 +0200

    created components and made folder structure

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+import sections from '../../data/directory.data';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: sections
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {this.state.sections.map(({ id, title, imageUrl, size }) => (
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Directory;
