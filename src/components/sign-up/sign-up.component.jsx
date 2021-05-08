c2c326e61bee33be38b2ee4b865e1b343ebfc4a2
Stefan Østergaard <Stefan.Ostergaard@tunstall.com>
2021-05-08 12:16:30 +0200

    implemented sign up with email and password

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import {
+  auth,
+  createUserProfileDocument,
+  doesUserExistWithEmail,
+} from "../../firebase/firebase.utils";
+
+import "./sign-up.styles.scss";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
+    try {
+      const userExists = await doesUserExistWithEmail(email);
+
+      if (!userExists) {
+        const { user } = await auth.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+
+        await createUserProfileDocument(user, { displayName });
+      } else {
+        alert("Email is already used");
+        return;
+      }
+
+      this.setState = {
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      };
+    } catch (err) {
+      console.log("User was not created", err);
+    }
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            handleChange={this.handleChange}
+            label="Display Name"
+            required
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            handleChange={this.handleChange}
+            label="Confirm Password"
+            required
+          />
+          <CustomButton type="submit"> SIGN UP </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,40 @@ const config = {
     appId: "1:67049790936:web:07ef2d50db954c7d29e6ad"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch(err) {
+      console.log('Error creating user', err);
+    }
+  }
+
+  return userRef;
+}
+
+export const doesUserExistWithEmail = async (email) => {
+  const collectioRef = await firestore.collection('users').where('email', '==', email).get();
+
+  return !collectioRef.empty;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
